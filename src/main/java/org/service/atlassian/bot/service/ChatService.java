8ac9d2459f465acbcb99c8be3659bc1c328a372e
Jeffrey <jeffreygunawan19@gmail.com>
2025-06-06 15:18:25 +0700

    feat: refactor JiraAssistant to return Result<String>

--- src/main/java/org/service/atlassian/bot/service/ChatService.java
+++ src/main/java/org/service/atlassian/bot/service/ChatService.java
@@ -1,5 +1,6 @@
 package org.service.atlassian.bot.service;
 
+import dev.langchain4j.service.Result;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.service.atlassian.bot.agent.JiraAssistant;
@@ -12,6 +13,7 @@ public class ChatService {
     private final JiraAssistant jiraAssistant;
 
     public String chat(String userPrompt, Integer userId) {
-        return jiraAssistant.chat(userPrompt);
+        Result<String> result = jiraAssistant.chat(userPrompt);
+        return result.content();
     }
 }
