c207ccd6a6945ca3b30a25a291d569de18ee7530
Jeffrey <jeffreygunawan19@gmail.com>
2025-06-22 14:55:54 +0700

    chore: revert ChatService back to constructor injection

--- src/main/java/org/service/atlassian/bot/service/ChatService.java
+++ src/main/java/org/service/atlassian/bot/service/ChatService.java
@@ -1,19 +1,16 @@
 package org.service.atlassian.bot.service;
 
-import dev.langchain4j.memory.chat.ChatMemoryProvider;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.Result;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.service.atlassian.bot.agent.JiraAssistant;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ChatService {
-    @Autowired
-    private JiraAssistant jiraAssistant;
+    private final JiraAssistant jiraAssistant;
 
     public String chat(String userPrompt, Integer userId) {
         Result<String> result = jiraAssistant.chat(userPrompt);
